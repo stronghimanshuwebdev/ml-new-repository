2765e4909668b3cf8bdd10adc8a817e7bb48a162
Himanshu kumar chaturvedi <chaturvedihimanshu120@gmail.com>
2024-02-04 01:30:43 +0530

    Updated

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from housing.logger import logging
 
 app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 
 def index():
+    logging.info("We are testing logging module")
     return "CI CD PIPELINE FOR PYTHON"
 
 if __name__ == '__main__':
