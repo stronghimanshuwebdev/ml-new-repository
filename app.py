d3554358c52246c5332eec78650fd459c0f92cd7
Himanshu kumar chaturvedi <chaturvedihimanshu120@gmail.com>
2024-02-04 16:36:57 +0530

    updated

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
+import sys
 from housing.logger import logging
+from housing.exception import HousingException
 
 app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 
 def index():
-    logging.info("We are testing logging module")
+    try:
+        raise Exception("We are testing custom exception ")
+    except Exception as e:
+        housing=HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
     return "CI CD PIPELINE FOR PYTHON"
 
 if __name__ == '__main__':
