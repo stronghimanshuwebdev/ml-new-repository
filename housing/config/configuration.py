5a418bdc8df3530e22c6b9943e9191070548d710
Himanshu kumar chaturvedi <chaturvedihimanshu120@gmail.com>
2024-02-10 15:11:51 +0530

    updated

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -3,13 +3,21 @@ ModelTrainerConfig,ModelEvaluationConfig,ModelPusherConfig,TrainingPipelineConfi
 
 from housing.utils.util import read_yaml_file
 from housing.constant import *
+import sys,os
+from housing.exception import HousingException
+from housing.logger import logging
 
 
 
 
 class Configuration:
-    def __init__(self)->None:
-        pass
+    def __init__(self,
+                 config_file_path=CONFIG_FILE_PATH,
+                 current_time_stamp=CURRENT_TIME_STAMP
+                 )->None:
+        self.config_info = read_yaml_file(file_path=config_file_path)
+        self.training_pipeline_config = self.get_training_pipeline_config()
+        self.time_stamp = current_time_stamp
 
     def get_data_ingestion_config(self)->DataIngestionConfig:
         pass
@@ -29,5 +37,15 @@ class Configuration:
     def get_model_pusher_config(self)->ModelPusherConfig:
         pass
     def get_training_pipeline_config(self)->TrainingPipelineConfig:
-        pass
+        try:
+            training_pipeline_config = self.config_info[TRAINING_PIPELINE_CONFIG_KEY]
+            artifact_dir = os.path.join(ROOT_DIR,
+            training_pipeline_config[TRAINING_PIPELINE_NAME_KEY],
+            training_pipeline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY])
+            training_pipeline_config = TrainingPipelineConfig(artifact_dir=artifact_dir)
+            logging.info(f"Training pipeline config :{training_pipeline_config}")
+            return training_pipeline_config
+
+        except Exception as e:
+            raise HousingException(e,sys) from e
 
