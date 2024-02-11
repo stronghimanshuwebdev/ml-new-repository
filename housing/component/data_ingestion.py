9292779667bc5481a75218b6f366d48820c604a9
Himanshu kumar chaturvedi <chaturvedihimanshu120@gmail.com>
2024-02-11 16:07:25 +0530

    updated

--- housing/component/data_ingestion.py
+++ housing/component/data_ingestion.py
@@ -5,6 +5,9 @@ from housing.entity.artifact_entity import DataIngestionArtifact
 from housing.logger import logging
 
 import tarfile
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import StratifiedShuffleSplit
 from six.moves import urllib
 
 class DataIngestion:
@@ -41,11 +44,68 @@ class DataIngestion:
         except Exception as e:
             raise HousingException(e,sys) from e
 
-    def extract_tgz_file(self):
-        pass
+    def extract_tgz_file(self,tgz_file_path:str):
 
-    def split_data_as_train_test(self):
-        pass
+        try:
+            raw_data_dir = self.data_ingestion_config.raw_data_dir
+            if os.path.exists(raw_data_dir):
+                os.remove(raw_data_dir)
+
+            os.makedirs(raw_data_dir,exist_ok=True)
+
+            logging.info(f"Extracting Tgz File: [{tgz_file_path}] into dir : [{raw_data_dir}]")
+
+            housing_tgz_file_obj = tarfile.open(tgz_file_path)
+
+            housing_tgz_file_obj.extractall(path=raw_data_dir)
+
+            logging.info(f"Extraction Completed")
+
+
+        except Exception as e:
+            raise HousingException(e,sys) from e
+
+
+
+
+    def split_data_as_train_test(self)->DataIngestionArtifact:
+        try:
+            raw_data_dir = self.data_ingestion_config.raw_data_dir
+            file_name = os.listdir(raw_data_dir)[0]
+
+            housing_file_path = os.path.join(raw_data_dir,file_name)
+
+            housing_data_frame = pd.read_csv(housing_file_path)
+
+            housing_data_frame["income_cat"] = pd.cut(
+                housing_data_frame["median_income"],
+                bins=[0.0,1.5,3.0,4.5,6.0,np.inf],
+                labels=[1,2,3,4,5]
+
+            )
+            strat_train_set = None
+            strat_test_split = None
+            split = StratifiedShuffleSplit(n_splits=1,test_size=0.2,random_state=42)
+
+            for train_index,test_index in split.split(housing_data_frame,housing_data_frame["income_cat"]):
+                strat_train_set = housing_data_frame.loc[train_index].drop("income_cat",axis=1)
+                strat_test_split = housing_data_frame.loc[test_index].drop(["income_cat"],axis=1)
+
+            train_file_path = os.path.join(self.data_ingestion_config.ingested_train_dir,
+                                           file_name)
+            
+            test_file_path = os.path.join(self.data_ingestion_config.ingested_test_dir,
+                                          file_name)
+            if strat_train_set is not None:
+                os.makedirs(self.data_ingestion_config.ingested_train_dir,exist_ok=True)
+                strat_train_set.to_csv(train_file_path,index=False)
+
+            if strat_test_split is not None:
+                os.makedirs(self.data_ingestion_config.ingested_test_dir,exist_ok=True)
+            
+ 
+        except Exception as e:
+            raise HousingException(e,sys) from e
 
 
         
@@ -53,6 +113,6 @@ class DataIngestion:
     def initiate_data_ingestion(self)-> DataIngestionArtifact:
         try:
             tgz_file_path = self.download_housing_data()
-            pass
+            self.extract_tgz_file(tgz_file_path=tgz_file_path)
         except Exception as e:
             raise HousingException(e,sys) from e
