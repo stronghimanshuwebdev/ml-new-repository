98152b35cbaaf4b5a5602fbb0d86dbee7946411d
Himanshu kumar chaturvedi <chaturvedihimanshu120@gmail.com>
2024-02-12 22:26:16 +0530

    updated

--- demo.py
+++ demo.py
@@ -1,8 +1,16 @@
 from housing.pipeline.pipeline import Pipeline
+from housing.exception import HousingException
+import sys
+from housing.logger import logging
 
 def main():
-    pipeline = Pipeline
-    pipeline.run_pipeline()
+    try:
+        pipeline = Pipeline()  # Create an instance of the Pipeline class
+        pipeline.run_pipeline()  # Call the run_pipeline method on the instance
+    except Exception as e:
+        logging.error(f"{e}")
+        print(e)
+        raise HousingException(e,sys) from e
 
 if __name__ == '__main__':
     main()
